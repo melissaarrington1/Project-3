2199cd4e52db113e2ee37e767d24b6342fcbdd38
Melissa Arrington <melissa.arrington1@yahoo.com>
2018-11-16 13:43:29 -0500

    more connectivity added

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,16 @@
 import React, { Component } from 'react';
 import './App.css';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+
 
 class App extends Component {
   render() {
     return (
+      <Router>
       <div>
         <h1>Hello from HomePage</h1>
       </div>
+      </Router>
     );
   }
 }
