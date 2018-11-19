09728a90d7a454295f86fb3bb3c5f016bc3245f2
Melissa Arrington <melissa.arrington1@yahoo.com>
2018-11-19 18:01:27 -0500

    delete button for user now works lol

--- client/src/Components/UserWeatherPage.js
+++ client/src/Components/UserWeatherPage.js
@@ -1,13 +1,42 @@
 import React, { Component } from 'react'
+import Button from '@material-ui/core/Button';
+import {Link} from 'react-router-dom'
+import Portal from '@material-ui/core/Portal';
+import axios from 'axios'
 
 class UserWeatherPage extends Component {
+    
+    state = {
+        city: '',
+        state: '',
+        currentTemp: ''
+    };
+    
+    handleDelete = () => {
+        axios.delete(`/api/users/${this.props.match.params.userId}`).then(res => {
+                this.props.history.push("/users");
+                })
+        }
+    
+
+    // onDelete() {
+    //     let userId = this.state.userId;
+    //     axios.delete(`http://localhost:3000/api/users/${userId}`)
+    //       .then(response => {
+    //         this.props.history.push("/");
+    //       })
+    //       .catch(err => console.log(err));
+    //   }
+
     render() {
         return(
-            <div>
+            <Portal>
                 <h1>Welcome</h1>
-                <h2>View Your Local Weather Below:</h2>
+                <h1>View Your Local Weather Below:</h1>
                 <h1>Select A City</h1>
-            </div>
+                {/* <button onClick={this.onDelete.bind(this)}>Delete User</button> */}
+                <Button onClick={this.handleDelete} variant="contained" color="primary"><Link to='/api/users/:userId'>Delete Your Account</Link></Button>
+            </Portal>
             
         )
     }
