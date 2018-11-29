1af3d72f7a0798b5f1579731dfc7636a9003e0c2
Melissa Arrington <melissa.arrington1@yahoo.com>
2018-11-29 17:19:30 -0500

    changed server

--- server.js
+++ server.js
@@ -1,36 +1,19 @@
-require("dotenv").config();
-const express = require('express');
-const bodyParser = require('body-parser');
-const mongoose = require('mongoose');
-const app = express();
+const express = require('express')
+const app = express()
 const routes = require('./routes/index')
-// mongoose.connect(process.env.MONGODB_URI); 
 
-const connection = mongoose.connection;
-connection.on('connected', () => {
-  console.log('Mongoose Connected Successfully')
-})
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
 
-// If the connection throws an error
-connection.on('error', (err) => {
-  console.log('Mongoose default connection error: ' + err);
-}) 
+app.use(express.static(__dirname + '/client/build/'))
 
-app.use(bodyParser.json());
-
-app.get('/', (req,res) => {
-  res.send('Hello world!')
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/client/build/index.html')
 })
 
 app.use('/', routes)
 
-app.use(express.static(__dirname + '/client/build/'));
-
-app.get('/', (req,res) => {
-    res.sendFile(__dirname + '/client/build/index.html')
-  })
-
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
-  console.log("Magic happening on port " + PORT);
+  console.log(`Server is listening on PORT: ${PORT}`)
 })
